de3122b1f2dfa1e11d1619bb70a02cf207c52abb
Justin-Barlowe <jbarlowe@my365.bellevue.edu>
2024-06-17 10:01:23 -0400

    Sign in page routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SigninComponent } from './security/signin/signin.component';
 
 export const routes: Routes = [
   {
@@ -9,6 +10,10 @@ export const routes: Routes = [
 {
   path: '',
   component: HomeComponent
+},
+{
+  path: 'signin',
+  component: SigninComponent
 }
 
 ];
